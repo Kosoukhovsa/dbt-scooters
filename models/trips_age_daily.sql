301c00b9e002075f2b80fa0eb2315a101546e670
Serg Kosoukhov <kosoukhovsa@gmail.com>
2026-05-12 18:10:16 +0300

    mission6_1

--- models/trips_age_daily.sql
+++ models/trips_age_daily.sql
@@ -1,8 +1,10 @@
-select 
-date(t.started_at) as date,
-extract( year from t.started_at) - extract( year from u.birth_date )  as age,
-count(*) as trips,
-sum(t.price_rub) as revenue_rub 
-from {{ref("trips_prep")}} as t  
-left join {{source("scooters_raw", "users")}} as u on t.user_id = u.id 
-group by 1, 2
+select
+    "date",
+    age,
+    count(*) as trips,
+    sum(price_rub) as revenue_rub
+from
+    {{ ref("trips_users") }}
+group by
+    1,
+    2
--- models/trips_stat.sql
+++ models/trips_stat.sql
@@ -1,9 +1,9 @@
-select 
-count(id) as trips,
-count( distinct user_id) as users,
-round(avg(duration_s) / 60, 2) as avg_duration_min,
-sum(price_rub) as revenue_rub ,
-count(*) filter(where is_free)::numeric / count(*) * 100 as free_trips_pct ,
-sum(distance_m) / 1000 as sum_distance_km 
-from {{ref("trips_prep")}}
-
+select
+    count(*) as trips,
+    count(distinct user_id) as users,
+    avg(duration_s) / 60 as avg_duration_min,
+    sum(distance_m) / 1000 as sum_distance_km,
+    sum(price_rub) as revenue_rub,
+    count(is_free or null) / cast(count(*) as real) * 100 as free_trips_pct
+from
+    {{ ref("trips_prep") }}
