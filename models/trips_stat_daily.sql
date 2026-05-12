76d80db71591673e8bda94f8d22760378e396648
Serg Kosoukhov <kosoukhovsa@gmail.com>
2026-05-12 18:19:06 +0300

    mission6_11

--- models/trips_stat_daily.sql
+++ models/trips_stat_daily.sql
@@ -1,9 +1,12 @@
-select 
-date,
-count(id) as trips,
-max(price_rub) / 100 as max_price_rub ,
-avg(distance_m) / 1000 as avg_distance_km ,
-sum(price_rub)/(sum(duration_s) / 60) as avg_price_rub_per_min
-from {{ref("trips_prep")}}
-group by 1
-order by 1
+select
+    "date",
+    count(*) as trips,
+    max(price_rub) as max_price_rub,
+    avg(distance_m) / 1000 as avg_distance_km,
+    avg(price_rub) / avg(duration_s) * 60 as avg_price_rub_per_min
+from
+    {{ ref("trips_prep") }}
+group by
+    1
+order by
+    1
